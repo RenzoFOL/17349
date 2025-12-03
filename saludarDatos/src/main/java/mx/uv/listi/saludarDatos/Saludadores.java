f6c82edbde178b03babc9b1835db70eef74eda7f
RenzoFOL <renzo.ortizlara@gmail.com>
2025-12-03 14:49:28 -0600

    base de datos railway docker

--- saludarDatos/src/main/java/mx/uv/listi/saludarDatos/Saludadores.java
+++ saludarDatos/src/main/java/mx/uv/listi/saludarDatos/Saludadores.java
@@ -8,22 +8,20 @@ import jakarta.persistence.Id;
 @Entity
 public class Saludadores {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
-
-    
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
     public Integer getId() {
         return id;
     }
     public void setId(Integer id) {
         this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
